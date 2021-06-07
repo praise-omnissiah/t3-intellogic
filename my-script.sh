c1ca55464af166c0f10ab89c199228c2054b9c64
dkexd <dmitrikondrasin@gmail.com>
2021-06-07 22:52:57 +0000

    fixed psql connection error

--- my-script.sh
+++ my-script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 #quit on fail
-set -euo pipefail
+#set -euo pipefail
 
 #create
 if [[ "$1" == "create" ]]; then
     #CREATE USER 'admin'@'localhost' IDENTIFIED BY 'my very strong password';
-    docker exec -d mysql bash -c '
+
+    docker exec mysql bash -c '
     mysql -u root -pexample -e "   
     CREATE DATABASE test; USE test;
     CREATE TABLE Costs (id VARCHAR(20) PRIMARY KEY, name VARCHAR(20), price DECIMAL);
@@ -15,11 +16,12 @@ if [[ "$1" == "create" ]]; then
 
     docker exec -d postgres bash -c '
     su postgres;
-    createuser postgres_user;
-    createdb -O postgres_user test;
-    psql -d test -c "CREATE TABLE Costs ( id VARCHAR PRIMARY KEY, name VARCHAR ( 20 ) UNIQUE NOT NULL, price DECIMAL);";
-    psql -d test -c "CREATE TABLE Products ( id VARCHAR PRIMARY KEY, name VARCHAR ( 20 ) UNIQUE NOT NULL, status VARCHAR ( 20 ), quantity CHAR( 1 ), priceId VARCHAR( 20 ));";
-    '
+    createuser -U postgres postgres_user;
+    createdb -U postgres -O postgres_user test;
+    psql -U postgres -c "
+    CREATE TABLE Costs ( id VARCHAR PRIMARY KEY, name VARCHAR ( 20 ) UNIQUE NOT NULL, price DECIMAL);
+    CREATE TABLE Products ( id VARCHAR PRIMARY KEY, name VARCHAR ( 20 ) UNIQUE NOT NULL, status VARCHAR ( 20 ), quantity CHAR( 1 ), priceId VARCHAR( 20 ));
+    "'
 fi
 
 #autoinsert
