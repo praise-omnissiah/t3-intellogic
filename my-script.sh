471b3d751e0f2eec0214bab4e64b917878c575f8
dkexd <dmitrikondrasin@gmail.com>
2021-06-08 13:32:00 +0000

    fixed volumes + gitignore

--- my-script.sh
+++ my-script.sh
@@ -10,23 +10,24 @@ if [[ "$1" == "create" ]]; then
     docker exec mysql bash -c '
     mysql -u root -pexample -e "   
     CREATE DATABASE test; USE test;
-    CREATE TABLE Costs (id VARCHAR(20) PRIMARY KEY, name VARCHAR(20), price DECIMAL);
-    CREATE TABLE Products (id VARCHAR(20) PRIMARY KEY, name VARCHAR(20), status VARCHAR(20), quantity CHAR(1), priceId VARCHAR(20));"
+    CREATE TABLE costs (id VARCHAR(20) PRIMARY KEY, name VARCHAR(20), price DECIMAL);
+    CREATE TABLE products (id VARCHAR(20) PRIMARY KEY, name VARCHAR(20), status VARCHAR(20), quantity CHAR(1), priceId VARCHAR(20));"
     '
 
     docker exec -d postgres bash -c '
-    su postgres;
     createuser -U postgres postgres_user;
     createdb -U postgres -O postgres_user test;
-    psql -U postgres -c "
-    CREATE TABLE Costs ( id VARCHAR PRIMARY KEY, name VARCHAR ( 20 ) UNIQUE NOT NULL, price DECIMAL);
-    CREATE TABLE Products ( id VARCHAR PRIMARY KEY, name VARCHAR ( 20 ) UNIQUE NOT NULL, status VARCHAR ( 20 ), quantity CHAR( 1 ), priceId VARCHAR( 20 ));
+    psql -U postgres -d test -c "
+    CREATE TABLE costs ( id VARCHAR PRIMARY KEY, name VARCHAR ( 20 ) UNIQUE NOT NULL, price DECIMAL);
+    CREATE TABLE products ( id VARCHAR PRIMARY KEY, name VARCHAR ( 20 ) UNIQUE NOT NULL, status VARCHAR ( 20 ), quantity CHAR( 1 ), priceId VARCHAR ( 20 ));
     "'
 fi
 
 #autoinsert
 if [[ "$1" == "autoinsert" ]]; then
-    docker exec -d postgres bash -c ''
+    docker exec -d postgres bash -c '
+    psql -U postgres_user -d test -c "INSERT INTO costs VALUES(RANDOM(), RANDOM();)"
+    '
 fi
 
 #migrate-10
