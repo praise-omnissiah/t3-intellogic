06568b185f4a7bfb235c5fbadeb0d7d2d556ace2
dkexd <dmitrikondrasin@gmail.com>
2021-06-08 17:26:34 +0000

    Fixed sql connection erro on create

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+#commands
 docker-compose up -d
 ./my-script.sh create
+./my-script.sh autoinsert
+./my-script.sh migrate-10
 docker-compose down
+
+# TO DO
+Remove creds from .sh
--- my-script.sh
+++ my-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #quit on fail
-#set -euo pipefail
+set -euo pipefail
 
 #create
 if [[ "$1" == "create" ]]; then
@@ -9,15 +9,13 @@ if [[ "$1" == "create" ]]; then
 
     docker exec mysql bash -c '
     mysql -u root -pexample -e "   
-    CREATE DATABASE test; USE test;
+    USE test;
     CREATE TABLE costs (id VARCHAR(20) PRIMARY KEY, name VARCHAR(20), price DECIMAL);
     CREATE TABLE products (id VARCHAR(20) PRIMARY KEY, name VARCHAR(20), status VARCHAR(20), quantity CHAR(1), priceId VARCHAR(20));"
     '
 
     docker exec -d postgres bash -c '
-    createuser -U postgres postgres_user;
-    createdb -U postgres -O postgres_user test;
-    psql -U postgres -d test -c "
+    psql -U postgres_user -d test -c "
     CREATE TABLE costs ( id VARCHAR PRIMARY KEY, name VARCHAR ( 20 ) UNIQUE NOT NULL, price DECIMAL);
     CREATE TABLE products ( id VARCHAR PRIMARY KEY, name VARCHAR ( 20 ) UNIQUE NOT NULL, status VARCHAR ( 20 ), quantity CHAR( 1 ), priceId VARCHAR ( 20 ));
     "'
